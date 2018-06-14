51dfab2b131e03500bf5cd96ef3a34534546c131
bo liu <liubo@luojilab.com>
2018-06-14 20:23:55 +0800

    更改首页样式

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const Koa = require('koa')
 const views = require('koa-views')
 const app = new Koa()
+const log4js = require('log4js')
+const logger = log4js.getLogger()
+logger.level = 'debug'
 
 app.use(views(__dirname + '/views'), {
   extension: 'html'
@@ -10,8 +13,8 @@ app.use(async (ctx) => {
   await ctx.render('index')
 })
 
-console.log('staring')
+logger.info('staring', new Date())
 
-app.listen(80)
+app.listen(8000)
 
 
